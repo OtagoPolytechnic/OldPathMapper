cdd217b2e04bb23a56dfc20719f96ee47714b168
tsgardiner <tsgardiner@windowslive.com>
2016-05-16 15:22:17 +1200

    app initial commit

---
+++ app/PathMapper/PathMapper/PathMapper/App.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PathMapper
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
