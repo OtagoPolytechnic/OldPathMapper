5634c2c14bd060251f8e9313c2a80150b0e096e4
tsgardiner <tsgardiner@windowslive.com>
2016-05-16 19:54:20 +1200

    Added Map Page Class
    Added known necessary permissions to manifest
    Attempted to create and add google maps api-key

---
+++ app/PathMapper/PathMapper/PathMapper/MapPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Maps;
+
+namespace PathMapper
+{
+    public class MapPage
+    {
+        Map map;
+
+        public MapPage()
+        {
+            map = new Map();
+        }
+
+    }
+}
