39a2c0c4ded6955a1420a3dab0f72f95c8a28977
acraig94 <acraig94@gmail.com>
2016-07-18 09:53:09 +1200

    GeoDjango Implementation
    Db changed from sqlite to postgis

--- admin/pathmapper/pathmapper/settings.py
+++ admin/pathmapper/pathmapper/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'pathmapperapp',
-    'geoposition',
+    
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -78,8 +79,10 @@ WSGI_APPLICATION = 'pathmapper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'pathmapperdb',
+        'USER': 'postgres',
+        'PASSWORD': 'pathmapper'
     }
 }
 
