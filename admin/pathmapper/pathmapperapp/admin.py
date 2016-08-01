c24b0b1d6cfc4cb06e81613878dd55fd73363cf9
acraig94 <acraig94@gmail.com>
2016-08-01 14:18:25 +1200

    Improved track model & admin

--- admin/pathmapper/pathmapperapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Plant, Collection, PlantCollection, Track
-
-admin.site.register(Plant)
-admin.site.register(Collection)
-admin.site.register(PlantCollection)
-admin.site.register(Track)
--- admin/pathmapper/pathmapperapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PathmapperappConfig(AppConfig):
-    name = 'pathmapperapp'
